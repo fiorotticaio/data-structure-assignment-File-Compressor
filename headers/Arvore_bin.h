ae57922992b144b51f9965c08b2d6968ba640203
Matheus <mt.schreiber01@gmail.com>
2022-07-17 15:51:39 -0300

    ajustando tad lista de aluno para lista de arvore

--- headers/Arvore_bin.h
+++ headers/Arvore_bin.h
@@ -16,14 +16,15 @@ typedef struct arv Arv;
 Arv* abb_cria_vazia(void);
 
 /** Cria árvore com subarvores esquerda e direita
- * input: caracter a ser inserido, ponteiro para vetor de pesos,
+ * input: variavel de controle (ehFolha) para algoritmo de Huffman
+ *        caracter a ser inserido, ponteiro para vetor de pesos,
  *        ponteiro para árvore esquerda, ponteiro para árvore direita
  * output: ponteiro para árvore
  * pré-condição: caracter válido, ponteiro para vetor de pesos existe,
  *               ponteiros para árvore esquerda e direita existem
  * pós-condição: ponteiro de retorno aponta para a árvore criada
  */
-Arv* abb_cria(char caracter, int* v, Arv* e, Arv* d);
+Arv* abb_cria(int ehFolha, char caracter, long int peso, Arv* e, Arv* d);
 
 /** Imprime árvore binária
  * input: ponteiro para árvore
@@ -33,35 +34,43 @@ Arv* abb_cria(char caracter, int* v, Arv* e, Arv* d);
  */
 void abb_imprime(Arv* a);
 
+/** Imprime árvore binária
+ * input: ponteiro para árvore
+ * output: nenhum
+ * pré-condição: ponteiro para a árvore existe
+ * pós-condição: nenhuma
+ */
+void abb_imprime_formato_graphviz(Arv * a);
+
 /** Busca por um caracter na árvore binária
  * input: ponteiro para a árvore, caracter a ser buscado,
- *        ponteiro para vetor de pesos
+ *        peso do caractere
  * output: nó da arovre que contém o caracter buscado (NULL caso não exista)
  * pré-condição: ponteiro para árvore existe, caracter válido,
  *               ponteiro para vetor de pesos existe
  * pós-condição: ponteiro de retorno aponta para o nó da árvore que contém o caracter buscado
  */
-Arv* abb_busca(Arv* a, char caracter, int* v);
+Arv* abb_busca(Arv* a, char caracter, long int peso);
 
 /** Insere um caracter na árovre binária
  * input: ponteiro para a árvore, caracter a ser inserido,
- *        ponteiro para vetor de pesos
+ *        peso do caractere
  * output: ponteiro para árovre com o caracter inserido
  * pré-condição: ponteiro para árvore existe, caracter válido,
  *               ponteiro para vetor de pesos existe
  * pós-condição: ponteiro de retorno aponta para árvore com o caracter adicionado
  */
-Arv* abb_insere(Arv* a, char caracter, int* v);
+Arv* abb_insere(Arv* a, char caracter, long int peso);
 
 /** Retira um caracter da árvore binária
  * input: ponteiro para a árvore, caracter a ser retirado,
- *        ponteiro para vetor de pesos
+ *        peso do caractere
  * output: ponteiro para árovre com o caracter retirado
  * pré-condição: ponteiro para árvore existe, caracter válido,
  *               ponteiro para vetor de pesos existe
  * pós-condição: ponteiro de retorno aponta para árvore agora sem o caracter
  */
-Arv* abb_retira(Arv* a, char caracter, int* v);
+Arv* abb_retira(Arv* a, char caracter, long int peso);
 
 /** Livra memória alocada para a árvore binária
  * input: ponteiro para árovre a ser liberada
@@ -70,5 +79,37 @@ Arv* abb_retira(Arv* a, char caracter, int* v);
  * pós-condição: toda a memória alocada para a árvore é liberada
  */
 Arv* abb_libera(Arv* a);
+
+/** Retorna o peso do caractere localizado em um nó
+ * input: ponteiro para arvore com o nó
+ * output: inteiro com valor do peso do caractere
+ * pré-condição: ponteiro para árvore existe
+ * pós-condição: arvore não é alterada
+ */
+long int getPeso(Arv * a);
+
+/** Retorna o caractere localizado em um nó
+ * input: ponteiro para arvore com o nó
+ * output: caractere
+ * pré-condição: ponteiro para árvore existe
+ * pós-condição: arvore não é alterada
+ */
+char getChar(Arv * a);
+
+/** Retorna o ponteiro para o ramo esquerdo da arvore
+ * input: ponteiro para arvore
+ * output: ponteiro para ramo esquerdo
+ * pré-condição: ponteiro para árvore existe
+ * pós-condição: arvore não é alterada
+ */
+Arv * getRamoEsq(Arv * a);
+
+/** Retorna o ponteiro para o ramo direito da arvore
+ * input: ponteiro para arvore
+ * output: ponteiro para ramo direito
+ * pré-condição: ponteiro para árvore existe
+ * pós-condição: arvore não é alterada
+ */
+Arv * getRamoDir(Arv * a);
 
 #endif // ARVORE_BIN
--- headers/Lista_arv.h
+++ headers/Lista_arv.h
@@ -1,20 +1,69 @@
 #ifndef LISTA_H
 #define LISTA_H
 
-#include "Aluno.h"
+#include "Arvore_bin.h"
 
+//Tipo Lista de Arvores
 typedef struct lista Lista;
 
+/** Cria uma Lista vazia
+ * input: nenhum
+ * output: ponteiro para a Lista vazia
+ * pré-condição: nenhuma
+ * pós-condição: ponteiro de retorno aponta para a lista vazia
+ */
 Lista* InicializaListaVazia();
 
-void DestroiLista(Lista* lista);
+/** Livra memória alocada para a lista
+ * input: ponteiro para lista a ser liberada
+ * output: ponteiro para a lista liberada
+ * pré-condição: ponteiro para lista existe
+ * pós-condição: toda a memória alocada para a lista é liberada
+ */
+void LiberaLista(Lista* lista);
 
+/** Imprime lista 
+ * input: ponteiro para lista
+ * output: nenhum
+ * pré-condição: ponteiro para a lista existe
+ * pós-condição: nenhuma
+ */
 void ImprimeLista(Lista* lista);
 
-void InsereAlunoUlt(Lista* lista, Aluno* aluno);
+/** Insere um caracter na lista
+ * input: ponteiro para a lista, árvore a ser inserido,
+ * output: ponteiro para lista com a árvore inserida
+ * pré-condição: ponteiro para lista e para árvore existem
+ * pós-condição: ponteiro de retorno aponta para lista com a árvore adicionada
+ */
+void InsereArvUlt(Lista* lista, Arv* arvore);
 
-void InsereAlunoPrim(Lista* lista, Aluno* aluno);
+//TODO: retirar futuramente?
+// void InsereArvPrim(Lista* lista, Arv* arvore);
 
-Aluno* RetiraAlunoLista(Lista* lista, char* nome);
+/** Retira uma árvore da lista
+ * input: ponteiro para a lista, árvore a ser retirada,
+ * output: ponteiro para árvore retirada
+ * pré-condição: ponteiro para lista existe, árvore válida,
+ *               ponteiro para vetor de pesos existe
+ * pós-condição: ponteiro de retorno aponta para lista agora sem a árvore
+ */
+Arv* RetiraArvLista(Lista* lista, Arv * arvore);
+
+/** Ordena a lista de arvore em relação aos pesos
+ * input: ponteiro para a lista
+ * output: nenhum
+ * pré-condição: ponteiro para lista existe
+ * pós-condição: lista agora está ordenada
+ */
+void OrdenaLista(Lista * lista);
+
+/** Aplica o algoritmo de Huffman para a lista de árvores
+ * input: ponteiro para a lista
+ * output: nenhum
+ * pré-condição: ponteiro para lista existe
+ * pós-condição: lista agora tem apenas um elemento que é uma árvore
+ */
+void Aplica_Huffman(Lista * lista);
 
 #endif // LISTA_H
